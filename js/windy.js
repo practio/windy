623806ae917961c3cb74273ee93fb1f5a77ca903
Jonatan Pedersen <jpe@practio.com>
2022-02-09 12:11:11 +0100

    adds dialogs

---
+++ js/windy.js
@@ -0,0 +1,24 @@
+document.addEventListener("click", handleClickEvent);
+
+function handleClickEvent(e) {
+  const { action, target } = e.target.dataset;
+
+  if (action === "show" || action === "showModal") {
+    let targetElement = document.querySelector(target);
+
+    if (!targetElement) {
+      return;
+    }
+
+    targetElement[action]();
+  } else if (action === "close") {
+    let targetElement =
+      document.querySelector(target) || e.target.closest("dialog");
+
+    if (!targetElement) {
+      return;
+    }
+
+    targetElement.close();
+  }
+}
