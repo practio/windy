fc08200598799c3cd1d60ff8543871ae7a201c68
Jonatan Pedersen <jpe@practio.com>
2022-03-09 10:08:25 +0100

    adds experimental navbar and dropdown

--- docs/index.f85bec1a.js
+++ docs/index.f85bec1a.js
@@ -1,39 +1,65 @@
-document.addEventListener("click", handleClickEvent);
-document.addEventListener("keydown", handleKeydownEvent);
-function handleKeydownEvent(e) {
-    if (e.key === "Escape") hideMenus();
+main();
+function main() {
+    menus();
+    modals();
 }
-function handleClickEvent(e) {
-    let action, target;
-    let element = e.target;
-    while(element && !action && !target){
-        action = element.dataset.action;
-        target = element.dataset.target;
-        element = element.parentElement;
+function menus() {
+    document.addEventListener("click", handleClickEvent);
+    document.addEventListener("keydown", handleKeydownEvent);
+    function handleKeydownEvent(e) {
+        if (e.key === "Escape") hideMenus();
     }
-    if (!element) hideMenus();
-    if (action === "show" || action === "showModal") {
-        let targetElement = document.querySelector(target);
-        if (!targetElement) return;
-        targetElement[action]();
-    } else if (action === "close") {
-        let targetElement = document.querySelector(target) || e.target.closest("dialog");
-        if (!targetElement) return;
-        if (targetElement.close) targetElement.close();
-    } else if (action === "remove") {
-        let targetElement = e.target.closest(target) || document.querySelector(target);
-        if (!targetElement) return;
-        if (targetElement.remove) targetElement.remove();
-    } else if (action === "toggle") {
-        hideMenus();
-        let targetElement = document.querySelector(target);
-        if (!targetElement) return;
-        targetElement.classList.toggle("hidden");
+    function handleClickEvent(e) {
+        let action, target;
+        const isClickOnElementInMenu = e.target.matches("[role=menu] *");
+        const isClickOnLinkInMenu = e.target.matches("[role=menu] a");
+        if (isClickOnElementInMenu && !isClickOnLinkInMenu) return;
+        let element = e.target;
+        while(element && !action && !target){
+            action = element.dataset.action;
+            target = element.dataset.target;
+            element = element.parentElement;
+        }
+        if (!element) {
+            hideMenus();
+            return;
+        }
+        if (action === "toggle") {
+            hideMenus();
+            let targetElement = document.querySelector(target);
+            if (!targetElement) return;
+            targetElement.classList.toggle("hidden");
+        }
+    }
+    function hideMenus() {
+        const menus1 = document.querySelectorAll("[role=menu]");
+        for (let menu of menus1)if (!menu.classList.contains("hidden")) menu.classList.add("hidden");
     }
 }
-function hideMenus() {
-    const menus = document.querySelectorAll("[role=menu]");
-    for (let menu of menus)if (!menu.classList.contains("hidden")) menu.classList.add("hidden");
+function modals() {
+    document.addEventListener("click", handleClickEvent);
+    function handleClickEvent(e) {
+        let action, target;
+        let element = e.target;
+        while(element && !action && !target){
+            action = element.dataset.action;
+            target = element.dataset.target;
+            element = element.parentElement;
+        }
+        if (action === "show" || action === "showModal") {
+            let targetElement = document.querySelector(target);
+            if (!targetElement) return;
+            targetElement[action]();
+        } else if (action === "close") {
+            let targetElement = document.querySelector(target) || e.target.closest("dialog");
+            if (!targetElement) return;
+            if (targetElement.close) targetElement.close();
+        } else if (action === "remove") {
+            let targetElement = e.target.closest(target) || document.querySelector(target);
+            if (!targetElement) return;
+            if (targetElement.remove) targetElement.remove();
+        }
+    }
 }
 
 //# sourceMappingURL=index.f85bec1a.js.map
--- js/windy.js
+++ js/windy.js
@@ -1,76 +1,112 @@
-document.addEventListener("click", handleClickEvent);
-document.addEventListener("keydown", handleKeydownEvent);
+main();
 
-function handleKeydownEvent(e) {
-  if (e.key === "Escape") {
-    hideMenus();
+function main() {
+  menus();
+  modals();
+}
+
+function menus() {
+  document.addEventListener("click", handleClickEvent);
+  document.addEventListener("keydown", handleKeydownEvent);
+
+  function handleKeydownEvent(e) {
+    if (e.key === "Escape") {
+      hideMenus();
+    }
+  }
+
+  function handleClickEvent(e) {
+    let action, target;
+
+    const isClickOnElementInMenu = e.target.matches("[role=menu] *");
+    const isClickOnLinkInMenu = e.target.matches("[role=menu] a");
+
+    if (isClickOnElementInMenu && !isClickOnLinkInMenu) {
+      return;
+    }
+
+    let element = e.target;
+
+    while (element && !action && !target) {
+      action = element.dataset.action;
+      target = element.dataset.target;
+      element = element.parentElement;
+    }
+
+    if (!element) {
+      hideMenus();
+
+      return;
+    }
+
+    if (action === "toggle") {
+      hideMenus();
+
+      let targetElement = document.querySelector(target);
+
+      if (!targetElement) {
+        return;
+      }
+
+      targetElement.classList.toggle("hidden");
+    }
+  }
+
+  function hideMenus() {
+    const menus = document.querySelectorAll("[role=menu]");
+
+    for (let menu of menus) {
+      if (!menu.classList.contains("hidden")) {
+        menu.classList.add("hidden");
+      }
+    }
   }
 }
 
-function handleClickEvent(e) {
-  let action, target;
+function modals() {
+  document.addEventListener("click", handleClickEvent);
 
-  let element = e.target;
+  function handleClickEvent(e) {
+    let action, target;
 
-  while (element && !action && !target) {
-    action = element.dataset.action;
-    target = element.dataset.target;
-    element = element.parentElement;
-  }
+    let element = e.target;
 
-  if (!element) {
-    hideMenus();
-  }
-
-  if (action === "show" || action === "showModal") {
-    let targetElement = document.querySelector(target);
-
-    if (!targetElement) {
-      return;
+    while (element && !action && !target) {
+      action = element.dataset.action;
+      target = element.dataset.target;
+      element = element.parentElement;
     }
 
-    targetElement[action]();
-  } else if (action === "close") {
-    let targetElement =
-      document.querySelector(target) || e.target.closest("dialog");
+    if (action === "show" || action === "showModal") {
+      let targetElement = document.querySelector(target);
 
-    if (!targetElement) {
-      return;
-    }
+      if (!targetElement) {
+        return;
+      }
 
-    if (targetElement.close) {
-      targetElement.close();
-    }
-  } else if (action === "remove") {
-    let targetElement =
-      e.target.closest(target) || document.querySelector(target);
+      targetElement[action]();
+    } else if (action === "close") {
+      let targetElement =
+        document.querySelector(target) || e.target.closest("dialog");
 
-    if (!targetElement) {
-      return;
-    }
+      if (!targetElement) {
+        return;
+      }
 
-    if (targetElement.remove) {
-      targetElement.remove();
-    }
-  } else if (action === "toggle") {
-    hideMenus();
+      if (targetElement.close) {
+        targetElement.close();
+      }
+    } else if (action === "remove") {
+      let targetElement =
+        e.target.closest(target) || document.querySelector(target);
 
-    let targetElement = document.querySelector(target);
+      if (!targetElement) {
+        return;
+      }
 
-    if (!targetElement) {
-      return;
-    }
-
-    targetElement.classList.toggle("hidden");
-  }
-}
-
-function hideMenus() {
-  const menus = document.querySelectorAll("[role=menu]");
-
-  for (let menu of menus) {
-    if (!menu.classList.contains("hidden")) {
-      menu.classList.add("hidden");
+      if (targetElement.remove) {
+        targetElement.remove();
+      }
     }
   }
 }
